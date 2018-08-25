673024d9051d4bb29155c3879b408df5960b98b2
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-08-26 08:20:24 +0900

    delete users location when user sign out

--- app/assets/javascripts/interval_location.js
+++ app/assets/javascripts/interval_location.js
@@ -5,15 +5,18 @@ var func = function location(){
       function(position) {
         $("#interval_latitude").val(position.coords.latitude)
         $("#interval_longitude").val(position.coords.longitude)
+        $("#interval_sub").trigger('click')
       },
       // error callback
       function(position) {
         alert( "can't get position" );
       }
     )
+  }else{
+    alert("can't get postion")
   }
 }
+func()
 setInterval(function(){
   func()
-  $("#interval_sub").trigger('click')
 }, 30000)
