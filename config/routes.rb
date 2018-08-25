bd37aee8d64b5ac54f9cea9a8d47f9d698161fd9
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-08-25 18:15:16 +0900

    add users show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
   }
+  resources :users, only: :show
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy" 
