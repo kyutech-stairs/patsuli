e3a462546c3c826660dd874cf7e4c69b5ebf0d92
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-08-25 20:39:40 +0900

    add refine tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,16 @@ class Tweet < ApplicationRecord
   validates :count, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+  
+  def self.refine_in(t, option={ d: 1})
+    a = []
+    d = option[:d]
+    all.each do |one|
+      if Geocoder::Calculations.distance_between([t.latitude, t.longitude], [one.latitude, one.longitude]) < d
+        a.push(one)
+      end
+    end
+    return a
+  end
+    
 end
