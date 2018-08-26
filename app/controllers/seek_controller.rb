241fe8c96cf05ad83b81604bcb34b7f8ce888ed7
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-08-26 11:17:28 +0900

    make change in seek controller

--- app/controllers/seek_controller.rb
+++ app/controllers/seek_controller.rb
@@ -1,9 +1,11 @@
 class SeekController < ApplicationController
+  def new
+
+  end
   def index
-    @tweets = Tweet.refine_in(current_user)
+    @tweets = Tweet.all
     # パラメータとして名前か性別を受け取っている場合は絞って検索する
-    if params[:wishing].present?
-      @tweets = @tweets.get_by_wishing params[:wishing]
-    end
+    @tweets = @tweets.get_by_wishing params[:wishing] if params[:wishing]
+    @tweets = @tweets.refine_in(current_user) if current_user.latitude
   end
 end
