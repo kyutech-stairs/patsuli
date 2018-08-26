662ca6f2ece5c745736d108377ded62cdd8e6c5a
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-08-26 12:13:55 +0900

    fix seek controller error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @tweets = Tweet.where(status: false)
-    @tweets = @tweets.refine_in(current_user) if current_user.latitude
+    if current_user
+      @tweets = @tweets.refine_in(current_user) if current_user.latitude
+    end
   end
 
   def authentication
