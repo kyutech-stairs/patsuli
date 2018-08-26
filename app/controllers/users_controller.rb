d3c23d080a0b675acac836bc19ff67e62e0840c6
EC2 Default User <ec2-user@ip-172-31-6-104.us-east-2.compute.internal>
2018-08-26 00:40:57 +0000

    page first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  
   def show 
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    @tweets = Tweet.all
   end
+  
+  
+  
+  
+  
 end
