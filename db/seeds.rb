8aeb51c4444cae522c76645dff9a254c8287c952
TakahiroSono <taka.sono889@gmail.com>
2018-08-26 09:31:24 +0900

    Merge branch 'master' of https://github.com/kyutech-stairs/patsuli

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ User.create!(email: "sample2@gmail.com",
             password: "password",
             password_confirmation: "password",
             name: "sono")
+User.create!(email: "sample3@gmail.com",
+            password: "password",
+            password_confirmation: "password",
+            name: "takeru")
+
+wish = ["ポテト", "かき氷", "焼き鳥", "水風船", "ビール", "佐世保バーガー"]
+20.times do |n|
+Tweet.create!(user_id: n%4+1,
+             reward: (n%6+1)*100,
+             count: n%5+1,
+             wishing: wish[n%6],
+             latitude: 33.591978499999996,
+             longitude: 130.39475629999998)
+end
